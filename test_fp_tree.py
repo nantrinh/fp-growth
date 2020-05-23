635e583c8e66029a982e19d5f3fa331e6bc0425a
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-05-23 13:02:30 -0700

    Add setUp and test_clean

---
+++ test_fp_tree.py
@@ -0,0 +1,57 @@
+import fp_tree
+import unittest
+from collections import defaultdict
+import ipdb
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def setUp(self):
+        transactions = [
+            [1, 2, 3, 4, 5, 6],
+            [2, 3, 4, 7, 8, 9],
+            [4, 7],
+            [3, 4]
+        ]
+        all_counts = {1: 1, 2: 2, 3: 3, 4: 4, 5: 1, 6: 1, 7: 2, 8: 1, 9: 1}
+        self.example = {
+            'transactions': transactions,
+            'all_counts': all_counts
+        }
+
+    def test_initial_count(self):
+        answer = defaultdict(lambda: 0)
+        for t in self.example['transactions']:
+            for item in t:
+                answer[item] += 1
+        self.assertEqual(
+            fp_tree.initial_count(
+                self.example['transactions']),
+            answer)
+
+    def test_prune(self):
+        sigma = 2
+        answer = {2: 2, 3: 3, 4: 4, 7: 2}
+        self.assertEqual(
+            fp_tree.prune(
+                self.example['all_counts'],
+                sigma),
+            answer)
+
+    def test_clean(self):
+        counts_with_min_support = {2: 2, 3: 3, 4: 4, 7: 2}
+        answer = [
+            [4, 3, 2],
+            [4, 3, 2, 7],
+            [4, 7],
+            [4, 3]
+        ]
+        self.assertEqual(
+            fp_tree.clean(
+                self.example['transactions'],
+                counts_with_min_support),
+            answer)
+
+
+if __name__ == '__main__':
+    unittest.main()
