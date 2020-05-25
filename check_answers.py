daf6b419f40f643bc57cde6949a7f7d531bf429d
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-05-25 14:00:39 -0700

    Check that patterns are unique in solution

--- check_answers.py
+++ check_answers.py
@@ -1,6 +1,6 @@
 import ipdb
 
-def valid_solution_line(line, min_length, min_support):
+def valid_solution_line(pattern_size, support, items, min_length, min_support):
     """
     line is an iterable in the format:
     pattern_size, support/count, item1, item2, ... 
@@ -10,20 +10,31 @@ def valid_solution_line(line, min_length, min_support):
     pattern_size >= min_length
     support >= min_support
     """
-    pattern_size, support, *items = line
-    pattern_size = int(pattern_size)
-    support = int(support)
-    
     assert pattern_size == len(items)
     assert pattern_size >= min_length
     assert support >= min_support
+    
 
 def valid_solution(filename, min_length, min_support):
     """
-    Reads in a file and checks if each line is a valid solution
+    Reads in a file and checks
+    - if each line is a valid solution
+    - that there are no duplicate patterns
     """
+    unique_items = set()
     for line in open(filename):
-        valid_solution_line(line.strip().split(', '), min_length, min_support)
+        pattern_size, support, *items = line.strip().split(', ')
+
+        pattern_size = int(pattern_size)
+        support = int(support)
+        items = tuple(items)
+
+        assert items not in unique_items
+        unique_items.add(items)
+
+        valid_solution_line(pattern_size, support, items, min_length, min_support)
+
+
 
 if __name__ == "__main__":
     valid_solution('slides_solution_1_2.txt', min_length=1, min_support=2)
