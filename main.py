888cd7eb96e9bf01bc97bb8b5b03da7f50e42e60
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-05-24 20:27:06 -0700

    Run with large file

--- main.py
+++ main.py
@@ -1,49 +1,48 @@
 import ipdb
-from collections import defaultdict
+
 import preprocess as pp
 import fp_tree
+import fp_helper
 
 
-def main(transactions, sigma=2):
+def main(transactions, min_support=4, min_length=3, file_location='output.txt'):
     # Preprocess transactions
     # Filter out those items that do not show up at least sigma times overall
     # Sort items per transaction in order of frequency (highest to lowest)
-    filtered_transactions, filtered_counts = pp.preprocess(transactions, sigma)
-
-    for t in filtered_transactions:
-        print(t)
-
-    for (k, v) in filtered_counts.items():
-        print(f'{k}: {v}')
+    filtered_transactions, filtered_counts = pp.preprocess(transactions, min_support)
 
     # Construct FPTree
     tree = fp_tree.FPTree() 
     for t in filtered_transactions:
         tree.add(t)
 
-    # Get prefix paths 
-    prefix_paths = tree.all_prefix_paths() 
-    for (k, v) in prefix_paths.items():
-        print(f'{k}: {v}')
-
-
-    # 
+    # Get prefix paths, taking min_length into consideration
+    # Get conditional fptree elements (longest common prefix)
+    # Generate frequent patterns
+    # Write to file
+    file_obj = open(file_location, 'w')
+    for item in filtered_counts:
+        print(f"Processing item {item}")
+        # pass is min_length - 1 as the min_length_of_prefix because the
+        # total length of a pattern will be prefix + item, or prefix + 1
+        prefix_paths = tree.prefix_paths(item, min_length - 1) 
+        conditional_fptree_elements = fp_helper.conditional_fptree_elements(prefix_paths, min_support)
+        print(f'{item}: {conditional_fptree_elements}')
+        frequent_patterns = fp_helper.frequent_patterns(item, conditional_fptree_elements)
+        fp_helper.write(file_obj, frequent_patterns)
+    file_obj.close()
     ipdb.set_trace()
 
 
 if __name__ == '__main__':
     # assuming data can fit in memory
-#    transactions = [list(map(int, line.split()))
-#                    for line in open('retail_25k.dat')]
-#    transactions = [
-#        [1, 2, 3, 4, 5, 6],
-#        [2, 3, 4, 7, 8, 9],
-#        [4, 7],
-#        [3, 4]
-#    ]
+    transactions = [list(map(int, line.split()))
+                    for line in open('retail_25k.dat')]
+    sigma = 4
+    min_length = 3
 
-    # https://www.geeksforgeeks.org/ml-frequent-pattern-growth-algorithm/
-    transactions = [
-        'ekmnoy', 'deknoy', 'aekm', 'ckmuy', 'ceikoo'
-    ]
-    main(transactions, 3)
+    import time
+    start = time.perf_counter()
+    main(transactions, sigma, min_length, 'retail_output.txt')
+    end = time.perf_counter()
+    print(f"Finished in {end - start:0.4f} seconds")
