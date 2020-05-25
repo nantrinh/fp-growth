fff8316e5bb4ff71b7dcc3e51f71d59ae58a8952
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-05-25 15:24:31 -0700

    Check output against a solution

--- check_answers.py
+++ check_answers.py
@@ -10,10 +10,13 @@ def valid_solution_line(pattern_size, support, items, min_length, min_support):
     pattern_size >= min_length
     support >= min_support
     """
-    assert pattern_size == len(items)
-    assert pattern_size >= min_length
-    assert support >= min_support
-    
+    try:
+        assert pattern_size == len(items)
+        assert pattern_size >= min_length
+        assert support >= min_support
+    except AssertionError as err:
+        print(err)
+        print(f'{pattern_size}, {support}, {items}. expected min_length: {min_length} min_support: {min_support}')
 
 def valid_solution(filename, min_length, min_support):
     """
@@ -34,10 +37,37 @@ def valid_solution(filename, min_length, min_support):
 
         valid_solution_line(pattern_size, support, items, min_length, min_support)
 
-
-
-if __name__ == "__main__":
+def check_slides_solutions():
     valid_solution('slides_solution_1_2.txt', min_length=1, min_support=2)
     valid_solution('slides_solution_2_2.txt', min_length=2, min_support=2)
     valid_solution('slides_solution_3_2.txt', min_length=3, min_support=2)
 
+def same_solution(solution, my_answer):
+    s_list = [] 
+    my_list = [] 
+
+    for line in open(solution):
+        pattern_size, support, *items = line.strip().split(', ')
+        s_list.append((pattern_size, support, set(items)))
+
+    for line in open(my_answer):
+        pattern_size, support, *items = line.strip().split(', ')
+        my_list.append((pattern_size, support, set(items)))
+
+    if len(my_list) != len(s_list):
+        print(f'my len: {len(my_list)}, expected len: {len(s_list)}')
+    for a in my_list:
+        if a not in s_list:
+            print('===')
+            print(a in s_list)
+            print(a)
+
+if __name__ == "__main__":
+    # check_slides_solutions()
+ #   import sys
+
+    # python check_answers.py output.txt 3 2
+#     filename, min_length, min_support = sys.argv[1:]
+#     min_length, min_support = int(min_length), int(min_support)
+#     valid_solution(filename, min_length, min_support)
+    same_solution('slides_solution_1_2.txt', 'output.txt')
