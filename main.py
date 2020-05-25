5c962424505dff2abe4c8e17ce7bb7c11b9bb625
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-05-24 20:38:03 -0700

    Prune for min_length_of_prefix in frequent_patterns

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import fp_tree
 import fp_helper
 
 
-def main(transactions, min_support=4, min_length=3, file_location='output.txt'):
+def main(transactions, min_support=1, min_length=1,
+         file_location='output.txt'):
     # Preprocess transactions
     # Filter out those items that do not show up at least sigma times overall
     # Sort items per transaction in order of frequency (highest to lowest)
-    filtered_transactions, filtered_counts = pp.preprocess(transactions, min_support)
+    filtered_transactions, filtered_counts = pp.preprocess(
+        transactions, min_support)
 
     # Construct FPTree
-    tree = fp_tree.FPTree() 
+    tree = fp_tree.FPTree()
     for t in filtered_transactions:
         tree.add(t)
 
@@ -23,16 +25,18 @@ def main(transactions, min_support=4, min_length=3, file_location='output.txt'):
     file_obj = open(file_location, 'w')
     for item in filtered_counts:
         print(f"Processing item {item}")
-        # pass is min_length - 1 as the min_length_of_prefix because the
+        # passing min_length - 1 as the min_length_of_prefix because the
         # total length of a pattern will be prefix + item, or prefix + 1
-        prefix_paths = tree.prefix_paths(item, min_length - 1) 
-        conditional_fptree_elements = fp_helper.conditional_fptree_elements(prefix_paths, min_support)
+        prefix_paths = tree.prefix_paths(item, min_length - 1)
+        conditional_fptree_elements = fp_helper.conditional_fptree_elements(
+            prefix_paths, min_support)
         print(f'{item}: {conditional_fptree_elements}')
-        frequent_patterns = fp_helper.frequent_patterns(item, conditional_fptree_elements)
+        # passing min_length - 1 as the min_length_of_prefix because the
+        # total length of a pattern will be prefix + item, or prefix + 1
+        frequent_patterns = fp_helper.frequent_patterns(
+            item, conditional_fptree_elements, min_length - 1)
         fp_helper.write(file_obj, frequent_patterns)
     file_obj.close()
-    ipdb.set_trace()
-
 
 if __name__ == '__main__':
     # assuming data can fit in memory
