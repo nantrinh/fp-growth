b82e68e8b87b0c3d4ed6f2a30c75ed1b4d741319
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-05-30 16:48:03 -0700

    Use conditional fp trees

--- main.py
+++ main.py
@@ -1,36 +1,22 @@
-import preprocess as pp
-import fp_tree
-import fp_helper
+from fp_growth import FPGrowth 
 
+def custom_format(itemset, support):
+    """pattern_size, support/count, item1, item2, ..."""
+    return f"{len(itemset)}, {support}, {', '.join(map(str, itemset))}"
 
-def main(transactions, min_length, min_support,
-         file_location='output.txt'):
-    # Preprocess transactions
-    # Filter out those items that do not show up at least sigma times overall
-    # Sort items per transaction in order of frequency (highest to lowest)
-    filtered_transactions, filtered_counts = pp.preprocess(
-        transactions, min_support)
+if __name__ == "__main__":
+    # transactions = [list(map(int, line.split()))
+    #                 for line in open('retail_25k.dat')]
+    # min_length = 3
+    # min_support = 4
 
-    # Construct FPTree
-    tree = fp_tree.FPTree()
-    for t in filtered_transactions:
-        tree.add(t)
+    # slides example
+    raw = ['ab', 'bcd', 'acde', 'ade', 'abc', 'abcd', 'a', 'abc', 'abd', 'bce']
+    transactions = [list(x) for x in raw]
+    min_length = 1
+    min_support = 2
 
-    # Write to file
-    file_obj = open(file_location, 'w')
+    fpg = FPGrowth(min_length=min_length, min_support=min_support)
 
-    # Generate frequent patterns
-    for (pattern, support) in fp_helper.frequent_patterns(tree, min_length, min_support):
-        # print(pattern, support)
-        fp_helper.write(file_obj, pattern, support)
-
-    file_obj.close()
-
-
-if __name__ == '__main__':
-    transactions = [list(map(int, line.split()))
-                    for line in open('retail_25k.dat')]
-    min_support = 4
-    min_length = 3
-
-    main(transactions, min_length, min_support)
+    for itemset, support in fpg.frequent_itemsets(transactions):
+        print(custom_format(itemset, support))
