b03343e2a99d0807fddf98e8b036399ed2742a97
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-05-30 21:11:45 -0700

    Remove comments

--- main.py
+++ main.py
@@ -24,21 +24,3 @@ if __name__ == "__main__":
         for res in run_fp(transactions, min_length=min_length, min_support=min_support):
             f.write(res + '\n')
         f.close()
-
-    # transactions = [list(map(int, line.split()))
-    #                 for line in open('retail_25k.dat')]
-    # min_length = 3
-    # min_support = 4
-
-    # slides example
-    # raw = ['ab', 'bcd', 'acde', 'ade', 'abc', 'abcd', 'a', 'abc', 'abd', 'bce']
-    # transactions = [list(x) for x in raw]
-    # raw = ['ekmnoy', 'deknoy', 'aekm', 'ckmuy', 'ceiko']
-    # transactions = [list(x) for x in raw]
-    # min_length = 1
-    # min_support = 2
-
-    # fpg = FPGrowth(min_length=min_length, min_support=min_support)
-
-    # for itemset, support in fpg.frequent_itemsets(transactions):
-    #     print(custom_format(itemset, support))
