fa7361aa629f4ed679dde800980c1f2a2c121d27
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-05-30 20:09:38 -0700

    Make it easier to debug

--- main.py
+++ main.py
@@ -4,19 +4,26 @@ def custom_format(itemset, support):
     """pattern_size, support/count, item1, item2, ..."""
     return f"{len(itemset)}, {support}, {', '.join(map(str, itemset))}"
 
-if __name__ == "__main__":
+def run_fp(transactions, min_length, min_support):
+    fpg = FPGrowth(min_length=min_length, min_support=min_support)
+    for itemset, support in fpg.frequent_itemsets(transactions):
+        yield custom_format(itemset, support)
+
+# if __name__ == "__main__":
     # transactions = [list(map(int, line.split()))
     #                 for line in open('retail_25k.dat')]
     # min_length = 3
     # min_support = 4
 
     # slides example
-    raw = ['ab', 'bcd', 'acde', 'ade', 'abc', 'abcd', 'a', 'abc', 'abd', 'bce']
-    transactions = [list(x) for x in raw]
-    min_length = 1
-    min_support = 2
+    # raw = ['ab', 'bcd', 'acde', 'ade', 'abc', 'abcd', 'a', 'abc', 'abd', 'bce']
+    # transactions = [list(x) for x in raw]
+    # raw = ['ekmnoy', 'deknoy', 'aekm', 'ckmuy', 'ceiko']
+    # transactions = [list(x) for x in raw]
+    # min_length = 1
+    # min_support = 2
 
-    fpg = FPGrowth(min_length=min_length, min_support=min_support)
+    # fpg = FPGrowth(min_length=min_length, min_support=min_support)
 
-    for itemset, support in fpg.frequent_itemsets(transactions):
-        print(custom_format(itemset, support))
+    # for itemset, support in fpg.frequent_itemsets(transactions):
+    #     print(custom_format(itemset, support))
