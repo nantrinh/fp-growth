b366d9feac4ed4ef0e721c6726b7cefb6478cb3a
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-05-30 21:09:52 -0700

    Add code to return 25k output

--- main.py
+++ main.py
@@ -9,7 +9,22 @@ def run_fp(transactions, min_length, min_support):
     for itemset, support in fpg.frequent_itemsets(transactions):
         yield custom_format(itemset, support)
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
+
+    prefix = 'retail_25k'
+    with open('retail_25k.dat') as f:
+        transactions = [list(map(int, line.split()))
+                    for line in f]
+
+    test_cases = [(3, 4)]
+
+    for min_length, min_support in test_cases: 
+        output_filename = f'outputs/{prefix}_{min_length}_{min_support}_output.txt'
+        f = open(output_filename, 'w')
+        for res in run_fp(transactions, min_length=min_length, min_support=min_support):
+            f.write(res + '\n')
+        f.close()
+
     # transactions = [list(map(int, line.split()))
     #                 for line in open('retail_25k.dat')]
     # min_length = 3
